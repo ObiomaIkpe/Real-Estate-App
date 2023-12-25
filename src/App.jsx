84c9d0010620e9f933c627186e66b305220a2bd6
ObiomaIkpe <obiomaikpe1.com>
2023-12-25 15:57:26 +0100

    setting up pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
-<h1 className='text-red-700'>App</h1>
+  <BrowserRouter>
+  <Routes>
+      <Route path='/' element={<Home />}  />
+      <Route path='/sign-in' element={<Signin />}  />
+      <Route path='/about' element={<About/>}  />
+      <Route path='/profile' element={<Profile />}  />
+      <Route path='/sign-up' element={<SignUp />}  />
+      
+
+
+  </Routes>
+  </BrowserRouter>
   )
 }
 
