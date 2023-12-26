9f333361211452d1faa2c3402696eebf96e53e98
ObiomaIkpe <obiomaikpe1.com>
2023-12-26 03:05:47 +0100

    connected to database

---
+++ backend/CONNECT/connectDB.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = (URL) => {
+    try {
+        console.log('connected to DB');
+        return mongoose.connect(URL)
+    }catch(err){
+        console.log(err)
+    }
+    
+} 
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
+import connectDB from './CONNECT/connectDB.js'
 
 const app = express();
 
-app.listen(4500, () => {
-    console.log('server is listening on port 4500')
-})
+
+const port = process.env.PORT || 4500;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        const server = app.listen(port, () => console.log(`listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
