9871260e77ea61748cf75e4d34d4fde679549189
ObiomaIkpe <obiomaikpe1.com>
2023-12-27 14:45:36 +0100

    redux toolkit for the frontend

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
-import {Link, useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 const SignUp = () => {
   const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(false)
+  // const [error, setError] = useState(null)
+  // const [loading, setLoading] = useState(false)
+
+  const {loading, error } = useSelector((state) => state.user);
+
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+
   const handleChange = (e) => {
     setFormData({
       ...formData, 
@@ -17,7 +24,8 @@ const SignUp = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+       //setLoading(true);
+       dispatch(signInStart())
     const res = await fetch('/api/auth/sign-in', 
     {
       method: 'POST',
@@ -30,18 +38,21 @@ const SignUp = () => {
     console.log(data)
 
     if(data.success === false){
-      setLoading(false);
-      setError(data.message);
+      // setLoading(false);
+      // setError(data.message);
+      dispatch(signInFailure(data.message))
       return;
     }
-    setError(null)
-    setLoading(false);
+    // setError(null)
+    // setLoading(false);
+    dispatch(signInSuccess(data))
 
     navigate('/');
    } catch (error) {
       console.log(error)
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message))
+      // setLoading(false);
+      // setError(error.message);
     }
     
     }
