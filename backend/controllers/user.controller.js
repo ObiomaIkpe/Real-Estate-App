6160ddb3eba803072973d0a7006d8164f608a2c3
ObiomaIkpe <obiomaikpe1.com>
2024-01-01 11:04:34 +0100

    get all listings API route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js"
 import bcryptjs from 'bcryptjs';
 import User from "../models/User.models.js";
+import Listing from '../models/listing.model.js'
 
 
 
@@ -57,6 +58,20 @@ export const deleteUser = async (req, res, next) => {
             message: 'user deleted succefully'
         })
     } catch (error) {
-        
+        next(error)
+    }
+}
+
+
+export const getUserListing = async (req, res, next) => {
+    if (req.user.id === req.params.id){
+        try {
+            const listings = await Listing.find({userRef: req.params.id});
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+    }else{
+        return next(errorHandler(401,'Unathorized!'))
     }
 }
