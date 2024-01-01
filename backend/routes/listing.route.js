675b6dc0aae3b0d2db662a1089581259535f6283
ObiomaIkpe <obiomaikpe1.com>
2024-01-01 23:00:54 +0100

    added delete user functionality

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import {createListing} from '../controllers/listing.controller.js';
+import {createListing, deleteListing} from '../controllers/listing.controller.js';
 import {verifyToken } from '../utils/verifyUser.js'
 
 router.route('/create').post(verifyToken, createListing);
+router.route('/delete/:id').delete(verifyToken, deleteListing);
+
 
 export default router 
 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -150,6 +150,23 @@ const handleShowListings = async () => {
 }
 
 
+const handleListingDelete =  async(listingId)  => {
+  try {
+    const res = await fetch(`/api/listing/delete/${listingId}`, {
+      method: 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -246,7 +263,10 @@ const handleShowListings = async () => {
             <p>{listing.name}</p>
         </Link>
         <div className="flex flex-col item-center gap-4"> 
-        <button className="text-red-700 uppercase mx-2 mt-2">Delete</button>
+        <button onClick={() => handleListingDelete(listing._id)} className="text-red-700 uppercase mx-2 mt-2">Delete</button>
+
+
+
         <button className="text-blue-700 uppercase">Edit</button>
         </div>
 
