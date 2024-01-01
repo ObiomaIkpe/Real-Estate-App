e067b555234f1ff5412db255651b55f6b8f94672
ObiomaIkpe <obiomaikpe1.com>
2024-01-01 23:18:46 +0100

    API route for updating listing

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import {createListing, deleteListing} from '../controllers/listing.controller.js';
+import {createListing, updateListing, deleteListing} from '../controllers/listing.controller.js';
 import {verifyToken } from '../utils/verifyUser.js'
 
 router.route('/create').post(verifyToken, createListing);
 router.route('/delete/:id').delete(verifyToken, deleteListing);
+router.route('/update/:id').patch(verifyToken, updateListing);
+
 
 
 export default router 
