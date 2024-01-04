f9e76623115e128cfd1aa4cc418c230ce0919daf
ObiomaIkpe <obiomaikpe1.com>
2024-01-04 23:18:21 +0100

    search-page UI

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,21 +1,100 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Search = () => {
+  const [sidebardata, setsidebardata] = useState({
+    searchTerm : '',
+    type : 'all',
+    parking: false,
+    furnished: false,
+    offer: false,
+    sort: 'created_at',
+    order: 'desc',
+
+  })
+
+  const handleChange = (e) => {
+    e.prevent
+  }
+
+
+
   return (
     <div className='flex flex-col md:flex-row md:min-h-screen'>
         <div className='p-7 border-b-2 md:border-r-2'>
-            <form>
+            <form className='flex flex-col gap-8'>
                 <div className='flex items-center gap-2'>
-                <label className='whitespace-nowrap'>Search Term:</label>
-                <input type='text' id='searchTerm' placeholder='search...' className='border rounded-lg p-3 w-full'></input>
+                <label className='whitespace-nowrap font-semibold'>Search Term:</label>
+                <input type='text' id='searchTerm' placeholder='search...' className='border rounded-lg p-3 w-full' value={sidebardata.searchTerm} onChange={handleChange}></input>
                 </div>
+
+
+                {/* handling types */}
+        <div className='flex gap-2 flex-wrap item-center'>
+          <label className='font-semibold'>Type: </label>
+            <div className='flex gap-2'>
+                <input type='checkbox' id='all' className='w-5'/>
+                <span>Rent & Sale</span>
+            </div>          
+        
+
+                
+            <div className='flex gap-2'>
+                <input type='checkbox' id='rent' className='w-5'/>
+                <span>Rent</span>
+            </div>          
+        
+
+
+        
+            <div className='flex gap-2'>
+                <input type='checkbox' id='sale' className='w-5'/>
+                <span>Sale</span>
+            </div>          
+        
+
+        
+            <div className='flex gap-2'>
+                <input type='checkbox' id='offer' className='w-5'/>
+                <span>Offer</span>
+            </div>          
+            </div>        
+
+
+            <div className='flex gap-2'>
+                <label className='font-semibold'>Amenities:</label>
+            <div className='flex gap-2'>
+                <input type='checkbox' id='parking' className='w-5'/>
+                <span>Parking</span>
+           </div>          
+             
+            <div className='flex gap-2'>
+                <input type='checkbox' id='furnished' className='w-5'/>
+                <span>Furnished</span>
+            </div>          
+        </div>     
+            
+
+            <div>
+              <label className='font-semibold'>Sort:</label>
+              <select id='sort_order' className='border rounded-lg p-3'>
+                <option>Price high to low</option>
+                <option>Price low to high</option>
+                <option>Latest</option>
+                <option>Oldest</option>
+              </select>
+            </div>
+
+            <button className='bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 uppercase'>Search</button>
+        
             </form>
         </div>
+            
 
-        <div className=''>
-            <h1>Listing Results:</h1>
+            <div className=''>
+            <h1 className='text-2xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing Results:</h1>
+            </div>
         </div>
-    </div>
+    
   )
 }
 
