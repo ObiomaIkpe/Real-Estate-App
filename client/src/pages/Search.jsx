0f4a634f1702e642abefc22c7f0e466cf8f13487
ObiomaIkpe <obiomaikpe1.com>
2024-01-05 02:05:55 +0100

    search functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const [listings, setListing] = useState([]);
+    console.log(listings)
   const [sidebardata, setsidebardata] = useState({
     searchTerm : '',
     type : 'all',
@@ -12,19 +17,108 @@ const Search = () => {
 
   })
 
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+
+    const searchTermFromUrl= urlParams.get('searchTerm');
+    const typeFromUrl = urlParams.get('type');
+    const parkingFromUrl = urlParams.get('parking');
+    const furnishedFromUrl = urlParams.get('furnished');
+    const offerFromUrl = urlParams.get('offer');
+    const sortFromUrl = urlParams.get('sort');
+    const orderFromUrl = urlParams.get('order');
+
+
+    if(
+      searchTermFromUrl ||
+      typeFromUrl ||
+      parkingFromUrl ||
+      furnishedFromUrl ||
+      offerFromUrl ||
+      sortFromUrl || 
+      orderFromUrl
+    ){
+      setsidebardata({
+        searchTerm: searchTermFromUrl || '',
+        type: typeFromUrl || 'all',
+        parking: parkingFromUrl === 'true' ? true : false,
+        furnished: furnishedFromUrl === 'true' ? true : false,
+        offer: offerFromUrl === 'true' ? true : false,
+        sort: sortFromUrl || 'created_at',
+        order: orderFromUrl || 'desc',
+      })
+
+    }
+
+
+    const fetchListings = async () => {
+        setLoading(true)
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        setListing(data);
+        setLoading(false)
+
+    }
+
+    fetchListings();
+
+  }, [location.search])
+
+  console.log(sidebardata);
+
   const handleChange = (e) => {
-    e.prevent
+    if(e.target.id === 'all' || e.target.id === 'rent' || e.target.id === 'sale'){
+      setsidebardata({...sidebardata, type: e.target.id})
+    }
+
+    if(e.target.id === 'searchTerm'){
+      setsidebardata({...sidebardata, searchTerm: e.target.value})
+    }
+
+    if (e.target.id === 'parking' || e.target.id === 'furnished' || e.target.id === 'offer') {
+      setsidebardata({...sidebardata, [e.target.id]: e.target.checked || e.target.checked === 'true' ? true : false})
+    }
+
+    if(e.target.id === 'sort_order'){
+      const sort = e.target.value.split('_')[0] || 'created_at';
+
+      const order = e.target.value.split('_')[1] || 'desc';
+
+      setsidebardata({ ...sidebardata, sort, order})
+
+    }
   }
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  // step 1: get the information that is already inside the URL.
+  const urlParams = new URLSearchParams();
+
+  // step 2: set URL params to what is already in the sidebardata object.
+  urlParams.set('searchTerm', sidebardata.searchTerm);
+  urlParams.set('type', sidebardata.type);
+  urlParams.set('parking', sidebardata.parking);
+  urlParams.set('furnished', sidebardata.furnished)
+  urlParams.set('offer', sidebardata.offer);
+  urlParams.set('order', sidebardata.order);
+  urlParams.set('sort', sidebardata.sort);
+  const searchQuery = urlParams.toString();
+  
+  navigate(`/search?${searchQuery}`);
+  
+}
 
 
 
   return (
     <div className='flex flex-col md:flex-row md:min-h-screen'>
         <div className='p-7 border-b-2 md:border-r-2'>
-            <form className='flex flex-col gap-8'>
+            <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
                 <div className='flex items-center gap-2'>
                 <label className='whitespace-nowrap font-semibold'>Search Term:</label>
-                <input type='text' id='searchTerm' placeholder='search...' className='border rounded-lg p-3 w-full' value={sidebardata.searchTerm} onChange={handleChange}></input>
+                <input type='text' id='searchTerm' placeholder='search...' className='border rounded-lg p-3 w-full' value={sidebardata.searchTerm} onChange={handleChange} />
                 </div>
 
 
@@ -32,14 +126,15 @@ const Search = () => {
         <div className='flex gap-2 flex-wrap item-center'>
           <label className='font-semibold'>Type: </label>
             <div className='flex gap-2'>
-                <input type='checkbox' id='all' className='w-5'/>
+                <input type='checkbox' id='all' className='w-5'checked={sidebardata.type === 'all'} onChange={handleChange}/>
                 <span>Rent & Sale</span>
             </div>          
         
 
                 
             <div className='flex gap-2'>
-                <input type='checkbox' id='rent' className='w-5'/>
+                <input type='checkbox' id='rent' className='w-5' 
+                checked={sidebardata.type === 'rent'} onChange={handleChange}/>
                 <span>Rent</span>
             </div>          
         
@@ -47,14 +142,15 @@ const Search = () => {
 
         
             <div className='flex gap-2'>
-                <input type='checkbox' id='sale' className='w-5'/>
+                <input type='checkbox' id='sale' className='w-5' onChange={handleChange} 
+                checked={sidebardata.type === 'sale'}/>
                 <span>Sale</span>
             </div>          
         
 
         
             <div className='flex gap-2'>
-                <input type='checkbox' id='offer' className='w-5'/>
+                <input type='checkbox' id='offer' className='w-5' onChange={handleChange} checked={sidebardata.offer}/>
                 <span>Offer</span>
             </div>          
             </div>        
@@ -63,12 +159,12 @@ const Search = () => {
             <div className='flex gap-2'>
                 <label className='font-semibold'>Amenities:</label>
             <div className='flex gap-2'>
-                <input type='checkbox' id='parking' className='w-5'/>
+                <input type='checkbox' id='parking' className='w-5' onChange={handleChange} checked={sidebardata.parking}/>
                 <span>Parking</span>
            </div>          
              
             <div className='flex gap-2'>
-                <input type='checkbox' id='furnished' className='w-5'/>
+                <input type='checkbox' id='furnished' className='w-5' onChange={handleChange} checked={sidebardata.furnished}/>
                 <span>Furnished</span>
             </div>          
         </div>     
@@ -76,11 +172,12 @@ const Search = () => {
 
             <div>
               <label className='font-semibold'>Sort:</label>
-              <select id='sort_order' className='border rounded-lg p-3'>
-                <option>Price high to low</option>
-                <option>Price low to high</option>
-                <option>Latest</option>
-                <option>Oldest</option>
+              <select onChange={handleChange} defaultValue={'created_at_desc'}
+              id='sort_order' className='border rounded-lg p-3'>
+                <option value='regularPrice_desc'>Price high to low</option>
+                <option value='regularPrice_asc'>Price low to high</option>
+                <option value='createdAt_desc'>Latest</option>
+                <option value='createdAt_asc'>Oldest</option>
               </select>
             </div>
 
